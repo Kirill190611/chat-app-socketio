9f3827b924177e5cca148066b9ba7df34ac5d8a9
Kirill190611 <k.igorevitch88@yandex.ru>
2024-10-07 20:24:36 +0300

    Added basic data for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react'
-import logo from './logo.svg'
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [messages, setMessages] = useState([
+    {
+      message: 'Hello Alex',
+      id: '23f2332',
+      user: { userId: '12as345', name: 'Kirill' },
+    },
+    {
+      message: 'Hello Kirill',
+      id: '45agf23',
+      user: { userId: '156gh32', name: 'Alex' },
+    },
+  ])
+
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          border: '1px solid black',
+          padding: '10px',
+          width: '300px',
+          height: '300px',
+          overflowY: 'scroll',
+        }}
+      >
+        {messages.map((message) => {
+          return (
+            <div key={message.id}>
+              <b>{message.user.name}:</b> {message.message}
+              <hr/>
+            </div>
+          )
+        })}
+      </div>
+      <textarea></textarea>
+      <button>Send</button>
     </div>
   )
 }
